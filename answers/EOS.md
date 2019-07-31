7b98d7ecc33423f32a96c5072715987056cc3043
RichardRed0x <37707106+RichardRed0x@users.noreply.github.com>
2019-07-31 16:07:16 +0100

    initial EOS proposals

---
+++ answers/EOS.md
@@ -0,0 +1,88 @@
+### Where does the governance happen?
+
+# Blockchain Governance
+
+- Does the project have its own blockchain or is it a token on another chain? (many questions are not relevant for tokens)
+
+- What is the mechanism for ensuring consensus about the state of the blockchain?
+
+  Consensus mechanism label:
+
+  - PoW
+  - PoS
+  - PoW/PoS Hybrid
+  - DPoS
+  - Byzantine Fault Tolerant
+  - Liquid-PoS?
+  - Master Nodes
+
+- Which entities interact with the blockchain? What role does each entity play? How are the "block producers" selected?
+
+- How are changes to the consensus rules implemented?
+
+- How were/are coins distributed? Was there an ICO? Is there inflation now? Is there a fixed supply?
+
+- Has the project been re-branded or re-named at any point?
+
+### Further reading 
+
+List good sources for further reading.
+
+# Funding
+
+- What is the reference node implementation?
+- Any other full node implementations?
+
+### How is development funded?
+
+- Donations
+- ICO
+- Block reward
+- Foundation/company
+- Grants
+- Not public/known
+
+##### How much or what proportion from each type of source?
+
+
+
+###### How are funds managed?
+
+- Non-profit foundation
+- For-profit company
+- Community voting/DAO mechanism
+
+
+
+#### What is the scope of funded work?
+
+
+
+
+
+### Related projects
+
+Are there any significant projects which are related? For example, is this a fork of another project? Have other projects forked this one? 
+
+
+
+
+
+### Significant Entities
+
+
+
+Name:
+
+Type: profit/non-profit
+
+Activity type: Investment, grant-making, product/service delivery
+
+Managed how:
+
+Founded when and by who
+
+Size: Resources, Employees
+
+Links to coins/chains other than the one in question
+
---
+++ governance-proposals/process-proposals.R
@@ -0,0 +1,58 @@
+library(ggplot2)
+library(lubridate)
+setwd("C:\\Users\\richa\\Documents\\GitHub\\crypto-governance-research\\governance-proposals")
+
+
+aragon = read.csv("Aragon-proposals.csv", stringsAsFactors = FALSE)
+aragon$eligible_voters = 39609523.80952380954
+
+aragon$total_votes = aragon$yes_votes+aragon$no_votes
+aragon$voter_participation = (aragon$total_votes/aragon$eligible_voters)*100
+write.csv(aragon, file = "Aragon-proposals.csv", row.names = FALSE)
+
+#write.csv(aragon, file = "Aragon-proposals.csv", row.names = FALSE)
+
+decred.politeia = read.csv("decred-proposals.csv", stringsAsFactors = FALSE)
+dash = read.csv("dash-proposals.csv", stringsAsFactors = FALSE)
+decred.consensus = read.csv("decred-proposals-onchain.csv", stringsAsFactors = FALSE)
+decred.consensus$total_votes = decred.consensus$yes_votes+decred.consensus$no_votes
+decred.consensus$voter_participation = (decred.consensus$total_votes/as.numeric(decred.consensus$eligible_voters))*100
+
+aragon$onchain = "no"
+dash$onchain = "yes"
+decred.politeia$onchain = "no"
+decred.consensus$onchain = "yes"
+
+decred = rbind(decred.politeia,decred.consensus)
+
+df = rbind(aragon,dash,decred.politeia, decred.consensus)
+df$approval = (df$yes_votes/df$total_votes)*100
+
+
+
+aragon$approval = (aragon$yes_votes/aragon$total_votes)*100
+
+#startdate is actually enddate
+#enddate can be taken from url
+
+eos = read.csv("eos-proposals.csv",stringsAsFactors = FALSE)
+#make voting_enddate proper
+eos$voting_enddate = as.character(eos$voting_enddate)
+eos$voting_enddate = as.POSIXct(eos$voting_enddate, format = "%d %b %Y %I:%M:%S %p")
+eos$voting_enddate = ymd_hms(eos$voting_enddate)
+
+eos$yes_votes = eos$total_votes*(eos$approval/100)
+eos$no_votes = eos$total_votes*((100-eos$approval)/100)
+eos$eligible_voters = 924995260
+eos$voter_participation = (eos$total_votes/eos$eligible_voters)*100
+eos$project = "eos"
+eos$onchain = "yes"
+
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+
+eos$startdate = substrRight(eos$url,8)
+eos$startdate = as.Date(eos$startdate, format = "%Y%m%d")
+
+
